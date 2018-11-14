c73196754c3a158682a8e11319a1b5ef3c138fe5
bingguang <bingguang@outlook.com>
2018-11-14 14:05:52 +0800

    add

--- CompareListwithDic/Program.cs
+++ CompareListwithDic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace CompareListwithDic
     {
         static void Main(string[] args)
         {
+            List<FileInfo> oldFileList = new DirectoryInfo("")
+                .GetFiles().Where(info => info.Extension.ToUpper() == ".TXT" 
+                && info.Name.ToLower().StartsWith("")).OrderByDescending(x => x.LastWriteTime).ToList();
+         
+        }
+        public static List<string> comparisonTextRow(string filePathNew, string filePathOld)
+        {
+            List<string> result = null;
+            if (string.IsNullOrWhiteSpace(filePathNew))
+            {
+                return result;
+            }
+            if (!File.Exists(filePathNew))
+            {
+                return result;
+            }
+            if (string.IsNullOrWhiteSpace(filePathOld) || !File.Exists(filePathOld))
+            {
+                return File.ReadAllLines(filePathNew).ToList();
+            }
+            else
+            {
+                List<string> oldTextRows = File.ReadAllLines(filePathOld).ToList();
+                List<string> newTextRows = File.ReadAllLines(filePathNew).ToList();
+                return newTextRows.Except(oldTextRows).ToList();
+            }
         }
     }
 }
